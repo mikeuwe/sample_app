1caa2fcf4af2bd83671afb228fe40b255aec493c
Mike <mike#@umbc.edu>
2012-03-31 11:50:21 -0400

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
 
 class User < ActiveRecord::Base
   attr_accessor :password
